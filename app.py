17807e74a61b6333120c00bc7ded560896dd5e79
Lee Xuan Ong <ongtan597@gmail.com>
2023-10-18 21:12:08 +0800

    initial code setup for flask server in app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import os
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+mysql = MySQL(app)
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)

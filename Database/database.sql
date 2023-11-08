546fd0d728c9ce536f66c0e987164439ca5c9dcc
kosystephy <kosysteve03@gmail.com>
2023-11-08 20:52:09 +0000

    populated SensorData table with sample data for testing

--- Database/database.sql
+++ Database/database.sql
@@ -32,6 +32,18 @@ VALUES
     (4,'GPS Module', 'Outdoor Garden');
 
 
+-- Sample data for the "SensorData" table with data_id
+INSERT INTO SensorData (data_id, sensor_id, timestamp, value)
+VALUES
+    (1, 1, '2023-10-26 08:00:00', 25.5),
+    (2, 2, '2023-10-26 09:30:00', 60.2),
+    (3, 3, '2023-10-26 11:15:00', 1005.3),
+    (4, 4, '2023-10-26 12:45:00', 800),
+    (5, 1, '2023-10-26 14:00:00', 26.0),
+    (6, 2, '2023-10-26 15:30:00', 59.8),
+    (7, 3, '2023-10-26 17:15:00', 1004.9),
+    (8, 4, '2023-10-26 18:45:00', 810);
+
 INSERT INTO users (user_id, name, password, role) VALUES
     (1, 'John Murphy', 'password123', 'admin'),
     (2, 'Jane Burns', 'userpass456', 'user'),

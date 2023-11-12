8f5f8524cca22388fb87f5a4ca6a8909bb675bd9
Lee Xuan Ong <ongtan597@gmail.com>
2023-11-12 16:25:17 +0000

    server confirm running with test data

--- aqua_guardians/app.py
+++ aqua_guardians/app.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, json, jsonify, make_response
 import os
+from dotenv import load_dotenv
+from chemflask import FlaskServerWrapper, EndpointHandler
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-
-mysql = MySQL(app)
 
 
+flask_app = Flask(__name__)
+app = FlaskServerWrapper(flask_app)
+# app.app.config.from_mapping(config)
+
+def create_response(json_data, status_code=200):
+    response = make_response(jsonify(json_data), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+def index():
+    users = [{"name": "John"}, {"name": "Jane"}]
+    return create_response(users)
 
 
+
+
+app.add_endpoint(endpoint="/",endpoint_name="index", handler=EndpointHandler(index), methods=['GET'])
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(debug=True)

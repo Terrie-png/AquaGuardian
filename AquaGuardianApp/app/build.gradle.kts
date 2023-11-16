beabc5efd23b810372a5373d2953160eb6e8802e
NojusMat <d00249674@student.dkit.ie>
2023-11-16 22:03:22 +0000

    Create main page with functional navigation to login in page

--- AquaGuardianApp/app/build.gradle.kts
+++ AquaGuardianApp/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.aquaguardianapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.aquaguardianapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

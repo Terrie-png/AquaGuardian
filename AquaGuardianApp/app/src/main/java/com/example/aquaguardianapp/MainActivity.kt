a96c59fe310db62bd62c6e8b7b9ef650f7b10e8a
NojusMat <d00249674@student.dkit.ie>
2023-11-18 11:52:06 +0000

    Create simple login page

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/MainActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/MainActivity.kt
@@ -20,16 +20,30 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +56,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
 
                 ) {
-                    Greeting("Android")
+                    Logins()
                 }
             }
         }
@@ -57,11 +71,10 @@ fun MyApp(modifier: Modifier = Modifier) {
         if (shouldShowOnboarding) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
-            Greetings()
+            Logins()
         }
     }
 }
-
 @Composable
 fun OnboardingScreen(
     onContinueClicked: () -> Unit,
@@ -78,6 +91,7 @@ fun OnboardingScreen(
             color = Color.White,
             fontFamily = FontFamily.Cursive
         )
+
     }
     Column(
         modifier = modifier.fillMaxSize(),
@@ -89,17 +103,18 @@ fun OnboardingScreen(
             contentDescription = "Aqua Guardian Logo",
             modifier = Modifier
                 .padding(bottom = 40.dp)
-                .size(200.dp)
-                .clip(CircleShape)
+                .size(400.dp)
+
         )
         Button(
             modifier = Modifier.padding(top = 40.dp),
             onClick = onContinueClicked
         ) {
-            Text(text = "Login",
+             Text(text = "Login",
                 fontSize = 50.sp,
                 fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
                 fontFamily = FontFamily.Cursive)
+
         }
         Button(
             modifier = Modifier.padding(vertical = 4.dp),
@@ -125,17 +140,90 @@ fun OnboardingScreen(
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun Greetings(
+fun PasswordTextField() {
+    var password by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Enter password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun loginTextBox() {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Username") },
+    )
+}
+
+@Composable                                                     
+private fun Logins(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Login", "Register")
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp).background(Color(0xFF00C2FF)),) {
-        for (name in names) {
-            Greeting(name = name)
+    Column(
+        modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Aqua Guardian",
+            modifier = Modifier.padding(top = 30.dp),
+            fontSize = 50.sp,
+            fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+            color = Color.White,
+            fontFamily = FontFamily.Cursive
+        )
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Aqua Guardian Logo",
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .size(200.dp)
+
+        )
+    }
+    Column(
+        modifier = modifier.padding(bottom = 20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        loginTextBox()
+        Spacer(modifier = Modifier.size(10.dp))
+        PasswordTextField()
+    }
+    Column( modifier = modifier.padding(bottom = 20.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            modifier = Modifier.padding(top = 20.dp),
+            onClick = {}
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 40.sp,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                fontFamily = FontFamily.Cursive
+            )
+
         }
     }
 }
+
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
@@ -146,7 +234,7 @@ fun OnboardingPreview() {
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun Login(name: String) {
 
     val expanded = remember { mutableStateOf(false) }
 
@@ -170,12 +258,11 @@ private fun Greeting(name: String) {
         }
     }
 }
-
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     AquaGuardianAppTheme {
-        Greetings()
+        Logins()
     }
 }
 

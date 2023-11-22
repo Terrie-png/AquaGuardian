5ed091daadce901d49aa199dab7a814db19e02c2
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-22 23:16:01 +0000

    Update RegisterActivity.kt
    Added the Firebase authentication for the users when registered.

--- AquaGuardianApp/app/build.gradle.kts
+++ AquaGuardianApp/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding=true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -62,6 +64,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RegisterActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RegisterActivity.kt
@@ -7,33 +7,87 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.util.Patterns
+import com.example.aquaguardianapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var etUsername: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var etPhoneNumber: EditText
-    private lateinit var etAddress: EditText
-    private lateinit var btnRegister: Button
+//    private lateinit var etUsername: EditText
+//    private lateinit var etPassword: EditText
+//    private lateinit var etEmail: EditText
+//    private lateinit var etPhoneNumber: EditText
+//    private lateinit var etAddress: EditText
+//    private lateinit var btnRegister: Button
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //connecting the .xml layout file to kotlin file
-        setContentView(R.layout.activity_register)
+//        //connecting the .xml layout file to kotlin file
+//        setContentView(R.layout.activity_register)
+//
+//        etUsername = findViewById(R.id.etUsername)
+//        etPassword = findViewById(R.id.etPassword)
+//        etEmail = findViewById(R.id.etEmail)
+//        etPhoneNumber = findViewById(R.id.etPhoneNumber)
+//        etAddress = findViewById(R.id.etAddress)
+//        btnRegister = findViewById(R.id.btnRegister)
+//
+//        btnRegister.setOnClickListener {
+//            registerUser()
+//        }
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        etUsername = findViewById(R.id.etUsername)
-        etPassword = findViewById(R.id.etPassword)
-        etEmail = findViewById(R.id.etEmail)
-        etPhoneNumber = findViewById(R.id.etPhoneNumber)
-        etAddress = findViewById(R.id.etAddress)
-        btnRegister = findViewById(R.id.btnRegister)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        btnRegister.setOnClickListener {
-            registerUser()
+        binding.btnRegister.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            val email = binding.etEmail.text.toString()
+            val phoneNumber = binding.etPhoneNumber.text.toString()
+            val address = binding.etAddress.text.toString()
+
+            // Check if any field is empty
+            if (username.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty() && phoneNumber.isNotEmpty() && address.isNotEmpty()) {
+                // Validate the email format
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(this, "Please enter a valid email address.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                // Validate the password length
+                if (password.length < 6) {
+                    Toast.makeText(
+                        this,
+                        "Password must be at least 6 characters long.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                // Validate the phone number
+                if (!Patterns.PHONE.matcher(phoneNumber).matches()) {
+                    Toast.makeText(this, "Please enter a valid phone number.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        Toast.makeText(this, "Registration successful.", Toast.LENGTH_SHORT).show()
+
+                    }
+                    else{
+                        Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please fill in all fields!", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
-    }
 
+    }
+}
 //    private fun registerUser() {
 //        val username= etUsername.text.toString().trim()
 //        val password= etPassword.text.toString().trim()
@@ -90,46 +144,46 @@ class RegisterActivity : AppCompatActivity() {
 //        Toast.makeText(this@RegisterActivity, "Registration successful!", Toast.LENGTH_LONG).show()
 //
 //    }
-private fun registerUser() {
-    // Retrieve the input from the EditTexts
-    val username = findViewById<EditText>(R.id.etUsername).text.toString().trim()
-    val password = findViewById<EditText>(R.id.etPassword).text.toString().trim()
-    val email = findViewById<EditText>(R.id.etEmail).text.toString().trim()
-    val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber).text.toString().trim()
-    val address = findViewById<EditText>(R.id.etAddress).text.toString().trim()
-
-    // Check if any field is empty
-    if (username.isEmpty() || password.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || address.isEmpty()) {
-        Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_SHORT).show()
-        return
-    }
-
-    // Validate the email format
-    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-        Toast.makeText(this, "Please enter a valid email address.", Toast.LENGTH_SHORT).show()
-        return
-    }
-
-    // Validate the password length
-    if (password.length < 6) {
-        Toast.makeText(this, "Password must be at least 6 characters long.", Toast.LENGTH_SHORT).show()
-        return
-    }
-
-    // Validate the phone number
-    if (!Patterns.PHONE.matcher(phoneNumber).matches()) {
-        Toast.makeText(this, "Please enter a valid phone number.", Toast.LENGTH_SHORT).show()
-        return
-    }
-
-    // If all validations pass, proceed with the registration process
-    // Since we do not have an actual backend, we'll just show a success message
-    Toast.makeText(this, "Registration successful.", Toast.LENGTH_SHORT).show()
-
-    // TODO: Implement actual registration logic here
-
-    // For now, we'll just finish the activity to simulate going back to the login screen
-    finish()
-}
-
-}
+//private fun registerUser() {
+//    // Retrieve the input from the EditTexts
+//    val username = findViewById<EditText>(R.id.etUsername).text.toString().trim()
+//    val password = findViewById<EditText>(R.id.etPassword).text.toString().trim()
+//    val email = findViewById<EditText>(R.id.etEmail).text.toString().trim()
+//    val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber).text.toString().trim()
+//    val address = findViewById<EditText>(R.id.etAddress).text.toString().trim()
+//
+//    // Check if any field is empty
+//    if (username.isEmpty() || password.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || address.isEmpty()) {
+//        Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_SHORT).show()
+//        return
+//    }
+//
+//    // Validate the email format
+//    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//        Toast.makeText(this, "Please enter a valid email address.", Toast.LENGTH_SHORT).show()
+//        return
+//    }
+//
+//    // Validate the password length
+//    if (password.length < 6) {
+//        Toast.makeText(this, "Password must be at least 6 characters long.", Toast.LENGTH_SHORT).show()
+//        return
+//    }
+//
+//    // Validate the phone number
+//    if (!Patterns.PHONE.matcher(phoneNumber).matches()) {
+//        Toast.makeText(this, "Please enter a valid phone number.", Toast.LENGTH_SHORT).show()
+//        return
+//    }
+//
+//    // If all validations pass, proceed with the registration process
+//    // Since we do not have an actual backend, we'll just show a success message
+//    Toast.makeText(this, "Registration successful.", Toast.LENGTH_SHORT).show()
+//
+//    // TODO: Implement actual registration logic here
+//
+//    // For now, we'll just finish the activity to simulate going back to the login screen
+//    finish()
+//}
+//
+//}

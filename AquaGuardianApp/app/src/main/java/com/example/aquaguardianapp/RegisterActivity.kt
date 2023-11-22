c78bf0eae93a6dff2a453132eb29485a7ec8e898
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-22 20:25:37 +0000

    Update RegisterActivity.kt
    Added the code for entering user details, basic input validation, and displaying a success or failure message.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RegisterActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RegisterActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import android.util.Patterns
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,8 +16,6 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var etPhoneNumber: EditText
     private lateinit var etAddress: EditText
     private lateinit var btnRegister: Button
-    private lateinit var tvLogin: TextView
-    private lateinit var tvLoginLink: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //connecting the .xml layout file to kotlin file
@@ -27,26 +27,109 @@ class RegisterActivity : AppCompatActivity() {
         etPhoneNumber = findViewById(R.id.etPhoneNumber)
         etAddress = findViewById(R.id.etAddress)
         btnRegister = findViewById(R.id.btnRegister)
-        tvLogin = findViewById(R.id.tvLogin)
-        tvLoginLink = findViewById(R.id.tvLoginLink)
 
         btnRegister.setOnClickListener {
             registerUser()
         }
-        tvLogin.setOnClickListener {
-            // Navigate to the login activity
-            // val intent = Intent(this, LoginActivity::class.java)
-            // startActivity(intent)
-        }
+
     }
 
-    private fun registerUser() {
-        val username= etUsername.text.toString().trim()
-        val password= etPassword.text.toString().trim()
-        val email = etEmail.text.toString().trim()
-        val phone = etPhoneNumber.text.toString().trim()
-        val address = etAddress.text.toString().trim()
+//    private fun registerUser() {
+//        val username= etUsername.text.toString().trim()
+//        val password= etPassword.text.toString().trim()
+//        val email = etEmail.text.toString().trim()
+//        val phone = etPhoneNumber.text.toString().trim()
+//        val address = etAddress.text.toString().trim()
+//
+//        //validate the inputs
+//        if (username.isEmpty()) {
+//            etUsername.error = "Username cannot be empty"
+//            return
+//        }
+//
+//        if (username.length < 3) {
+//            etUsername.error = "Username must be at least 3 characters long"
+//            return
+//        }
+//
+//        if (password.isEmpty()) {
+//            etPassword.error = "Password cannot be empty"
+//            return
+//        }
+//
+//        if (password.length < 8) {
+//            etPassword.error = "Password must be at least 8 characters long"
+//            return
+//        }
+//
+//        if (email.isEmpty()) {
+//            etEmail.error = "Email cannot be empty"
+//            return
+//        }
+//
+//        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            etEmail.error = "Invalid email address"
+//            return
+//        }
+//
+//        if (phone.isEmpty()) {
+//            etPhoneNumber.error = "Phone number cannot be empty"
+//            return
+//        }
+//
+//        if (!android.util.Patterns.PHONE.matcher(phone).matches()) {
+//            etPhoneNumber.error = "Invalid phone number"
+//            return
+//        }
+//
+//        if (address.isEmpty()) {
+//            etAddress.error = "Address cannot be empty"
+//            return
+//        }
+//        //Success message
+//        Toast.makeText(this@RegisterActivity, "Registration successful!", Toast.LENGTH_LONG).show()
+//
+//    }
+private fun registerUser() {
+    // Retrieve the input from the EditTexts
+    val username = findViewById<EditText>(R.id.etUsername).text.toString().trim()
+    val password = findViewById<EditText>(R.id.etPassword).text.toString().trim()
+    val email = findViewById<EditText>(R.id.etEmail).text.toString().trim()
+    val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber).text.toString().trim()
+    val address = findViewById<EditText>(R.id.etAddress).text.toString().trim()
 
-        //validate
+    // Check if any field is empty
+    if (username.isEmpty() || password.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || address.isEmpty()) {
+        Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_SHORT).show()
+        return
     }
+
+    // Validate the email format
+    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        Toast.makeText(this, "Please enter a valid email address.", Toast.LENGTH_SHORT).show()
+        return
+    }
+
+    // Validate the password length
+    if (password.length < 6) {
+        Toast.makeText(this, "Password must be at least 6 characters long.", Toast.LENGTH_SHORT).show()
+        return
+    }
+
+    // Validate the phone number
+    if (!Patterns.PHONE.matcher(phoneNumber).matches()) {
+        Toast.makeText(this, "Please enter a valid phone number.", Toast.LENGTH_SHORT).show()
+        return
+    }
+
+    // If all validations pass, proceed with the registration process
+    // Since we do not have an actual backend, we'll just show a success message
+    Toast.makeText(this, "Registration successful.", Toast.LENGTH_SHORT).show()
+
+    // TODO: Implement actual registration logic here
+
+    // For now, we'll just finish the activity to simulate going back to the login screen
+    finish()
+}
+
 }

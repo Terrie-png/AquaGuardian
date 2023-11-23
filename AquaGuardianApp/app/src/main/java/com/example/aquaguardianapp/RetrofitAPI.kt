ed8ad0639008c9b869e033e72518e01124141bd4
NojusMat <d00249674@student.dkit.ie>
2023-11-23 13:44:33 +0000

    Implemented simple validation for login page

---
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RetrofitAPI.kt
@@ -0,0 +1,16 @@
+package com.example.aquaguardianapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface RetrofitAPI {
+    // as we are making get request so we are displaying
+    // GET as annotation.
+    // and inside we are passing last parameter for our url.
+    @GET("T7R2")
+    fun
+    // as we are calling data from array so we are calling
+    // it with array list and naming that method as getAllCourses();
+            getLanguages(): Call<ArrayList<ListModal>>
+
+}

3b10b120664d62e95cf7fe564f8348356fdb6964
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-24 15:56:34 +0000

    Create ServiceBuilder.kt
    Created the client and the retrofit instance to communicate between the app and the server.

---
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/ServiceBuilder.kt
@@ -0,0 +1,21 @@
+package com.example.aquaguardianapp
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object ServiceBuilder {
+    private const val baseUrl = "https://reqres.in/"
+
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+    fun<T> buildService(service:Class<T>):T {
+        return  retrofit.create(service)
+    }
+}

608b1e175aa50bed28264179d026462520168023
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-24 21:49:22 +0000

    Update User.kt

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/User.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/User.kt
@@ -3,9 +3,8 @@ package com.example.aquaguardianapp
 data class User(
     val username: String,
     val name: String,
-    val password: String,
     val email: String,
     val phone: String,
     val address: String,
-    val userUID: String
+    val userUID: String?
 )

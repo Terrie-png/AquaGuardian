30c2b1bc586836e7358bc9962385ea0f1257be28
Lee Xuan Ong <ongtan597@gmail.com>
2023-11-24 10:48:54 +0000

    final update integration

--- Server/main.py
+++ Server/main.py
@@ -1,27 +1,22 @@
+import threading
 from src.functions import app, channel, pnb
-from src.controllers import activePubnub
+from src.controllers import MySubscribeCallback
 from src.routes import api_v1
+from threading import Thread
 import sys
 import signal
 import atexit
 
 app.register_blueprint(api_v1)
 
-def signal_handler(sig, frame):
-    print("Ctrl+C pressed. Cleaning up...")
-    sys.exit(0)
+def active_pubnub():
+    pnb.add_listener(MySubscribeCallback())
+    pnb.subscribe().channels(channel).execute()
 
-# Register the exit function
-atexit.register(pnb.stop)
 
-# Register the signal handler for Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
-
-# Start PubNub
-activePubnub()
 # where i start the server
 if __name__ == '__main__':
-    activePubnub()
-    signal.signal(signal.SIGINT, signal_handler)
-    app.run(host='0.0.0.0',debug=True)
+    Thread(target=active_pubnub).start()
+    app.run(host='0.0.0.0',debug=True, port=5000)
+    
     

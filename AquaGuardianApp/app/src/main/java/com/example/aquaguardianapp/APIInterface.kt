e4d0a7aaef16b7cd2036a6c908e8f9c035fbf36f
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-25 19:59:22 +0000

    Update APIInterface.kt

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/APIInterface.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/APIInterface.kt
@@ -5,6 +5,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface APIInterface {
-    @POST("")
+    @POST("/register")
     fun registerUser(@Body user: User): Call<RegistrationResponse>
 }

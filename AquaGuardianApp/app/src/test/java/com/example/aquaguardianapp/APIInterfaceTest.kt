437b072c4c8c74f52280aa729285c5c51640e247
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-25 19:57:59 +0000

    Create APIInterfaceTest.kt
    Added the basic to test the API calls between the app the local server.

---
+++ AquaGuardianApp/app/src/test/java/com/example/aquaguardianapp/APIInterfaceTest.kt
@@ -0,0 +1,84 @@
+package com.example.aquaguardianapp
+
+import Helper
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+
+class APIInterfaceTest {
+
+    lateinit var mockWebServer: MockWebServer
+    lateinit var apiInterface: APIInterface
+
+    @Before
+    fun setup() {
+        mockWebServer = MockWebServer()
+        apiInterface = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(APIInterface::class.java)
+    }
+
+    @Test
+    fun testRegisterUser(){
+        val mockResponse = MockResponse()
+        mockResponse.setBody("""{
+         "success": true
+    }""")
+
+        mockWebServer.enqueue(mockResponse)
+        // Create a mock User object
+        val mockUser = User(
+            username = "testuser",
+            name = "Test User",
+            email = "test@example.com",
+            phone = "1234567890",
+            address = "123 Test Street",
+            userUID = null
+        )
+        val response = apiInterface.registerUser(mockUser).execute()
+
+        // Assert the expected properties of the response
+        val responseBody = response.body()
+        Assert.assertTrue("Response should indicate success", responseBody?.success == true)
+    }
+
+    @Test(timeout = 10000) // Timeout of 10 seconds
+    fun testRegisterUser_returnSuccessResponse() {
+        try {
+            val content = Helper.readFileResource("C:/Users/rathn/AndroidStudioProjects/AquaGuardian/AquaGuardianApp/app/src/test/resources/response.json")
+            val mockResponse = MockResponse()
+            mockResponse.setResponseCode(200)
+            mockResponse.setBody(content)
+            mockWebServer.enqueue(mockResponse)
+
+            val mockUser = User(
+                username = "testuser",
+                name = "Test User",
+                email = "test@example.com",
+                phone = "1234567890",
+                address = "123 Test Street",
+                userUID = null
+            )
+
+            val response = apiInterface.registerUser(mockUser).execute()
+
+            val responseBody = response.body()
+            Assert.assertTrue("Response should indicate success", responseBody?.success == true)
+        }
+        catch (e: Exception){
+            Assert.fail("Error reading resource file: ${e.message}")
+        }
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+}

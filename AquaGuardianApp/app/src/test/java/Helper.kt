8ae437907a4adf93aa79db18e83c2cb2b9197fce
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-25 22:21:15 +0000

    Delete Helper.kt
    Wrong Location.

--- AquaGuardianApp/app/src/test/java/Helper.kt
+++
@@ -1,8 +0,0 @@
-import java.io.InputStreamReader
-
-object Helper {
-    fun readFileResource(path: String): String {
-        return this::class.java.getResourceAsStream(path)?.bufferedReader().use { it?.readText() }
-            ?: throw IllegalArgumentException("Resource not found: $path")
-    }
-}

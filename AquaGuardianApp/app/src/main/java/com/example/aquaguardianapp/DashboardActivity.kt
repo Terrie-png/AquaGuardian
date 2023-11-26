8436a6fba17f17bb4368410bba1f2a3d15d6d5ca
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-26 02:33:23 +0000

    Update DashboardActivity.kt
    Added the code to navigate to ActiveDevices Page where the water quality is displayed.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/DashboardActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/DashboardActivity.kt
@@ -1,11 +1,26 @@
 package com.example.aquaguardianapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aquaguardianapp.databinding.ActivityDashboardBinding
+import com.example.aquaguardianapp.databinding.ActivityMainBinding
+import java.lang.StringBuilder
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
+        binding= ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btActiveDevices.setOnClickListener{
+            //Create an Intent to start the WaterQuality page
+            val intent = Intent(this,WaterQualityActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }

ab466dc6a1c5f6613719df2b81404c964c721ade
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-26 02:30:19 +0000

    Update WaterQualityActivity.kt
    Added the code to display water quality.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/WaterQualityActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/WaterQualityActivity.kt
@@ -2,10 +2,67 @@ package com.example.aquaguardianapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.aquaguardianapp.databinding.ActivityRegisterBinding
+import com.example.aquaguardianapp.databinding.ActivityWaterQualityBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class WaterQualityActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWaterQualityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_water_quality)
+
+        binding=ActivityWaterQualityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCheckWaterQuality.setOnClickListener{
+            checkWaterQuality()
+        }
+    }
+
+    private fun checkWaterQuality() {
+//        // Logic to check water quality
+//        // This could involve fetching data from a sensor or an API
+//        // For now, let's just simulate the check with a hardcoded value
+//        val isDrinkable = false // Replace with actual water quality check logic
+
+        val serviceBuilder = ServiceBuilder.buildService(APIInterface::class.java)
+        val call = serviceBuilder.checkWaterQuality()
+
+        call.enqueue(object : Callback<WaterQualityResponse> {
+            override fun onResponse(call: Call<WaterQualityResponse>, response: Response<WaterQualityResponse>) {
+                if (response.isSuccessful) {
+                    val isDrinkable= response.body()?.quality
+                    if (isDrinkable != null) {
+                        updateWaterQuality(isDrinkable)
+                    }
+                } else {
+                    Toast.makeText(this@WaterQualityActivity,"No water quality data received", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<WaterQualityResponse>, t: Throwable) {
+                // Handle network failure or other errors
+                Toast.makeText(this@WaterQualityActivity,"Failed to register. Please check your network connection and try again.", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+    }
+
+    private fun updateWaterQuality(isDrinkable: Boolean) {
+        if (isDrinkable) {
+            // Show green happy emoji and corresponding message with green text color
+            binding.imgEmoji.setImageResource(R.drawable.happyface)
+            binding.txtWaterQualityMessage.text = getString(R.string.water_quality_good)
+            binding.txtWaterQualityMessage.setTextColor(ContextCompat.getColor(this, R.color.green))
+        } else {
+            // Show red sad emoji and corresponding message with red text color
+            binding.imgEmoji.setImageResource(R.drawable.sadface)
+            binding.txtWaterQualityMessage.text = getString(R.string.water_quality_bad)
+            binding.txtWaterQualityMessage.setTextColor(ContextCompat.getColor(this, R.color.red))
+        }
     }
 }

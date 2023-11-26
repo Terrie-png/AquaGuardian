fd06bf5f636e0c1abaf6494ff8d144584506b012
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-26 02:31:29 +0000

    Update RegisterResponse.kt

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RegistrationResponse.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/RegistrationResponse.kt
@@ -2,6 +2,5 @@ package com.example.aquaguardianapp
 
 data class RegistrationResponse(
     val success: Boolean,
-    val userId: Int? = null,
-    val name: String,
-    val userUID: String)
+    val userUID: String // not needed ig
+)

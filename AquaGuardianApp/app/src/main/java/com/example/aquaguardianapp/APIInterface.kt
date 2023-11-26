f342bbc6a20466434de66ab65a10eb2e234a7812
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-26 02:26:39 +0000

    Update APIInterface.kt
    Added the endpoints from mockable.io for a POST registration request and a Get water quality request.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/APIInterface.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/APIInterface.kt
@@ -2,9 +2,13 @@ package com.example.aquaguardianapp
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface APIInterface {
-    @POST("/register")
+    @POST("register")
     fun registerUser(@Body user: User): Call<RegistrationResponse>
+
+    @GET("waterQuality")
+    fun checkWaterQuality(): Call<WaterQualityResponse>
 }

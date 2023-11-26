d5c56a2f4c98a43d092b75e949c2f9f7a9a67a81
Raveena Rathnam <D00251856@student.dkit.ie>
2023-11-26 02:32:07 +0000

    Update MainActivity.kt
    Changed to binding.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/MainActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/MainActivity.kt
@@ -13,18 +13,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.aquaguardianapp.databinding.ActivityMainBinding
+import com.example.aquaguardianapp.databinding.ActivityWaterQualityBinding
 import com.example.aquaguardianapp.ui.theme.AquaGuardianAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // Initialize the TextView for registration link
-        val tvRegisterLink = findViewById<TextView>(R.id.tvRegisterLink)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //Set an onClickListener on the TextView
-        tvRegisterLink.setOnClickListener{
+        binding.tvRegisterLink.setOnClickListener{
             //Create an Intent to start the RegisterActivity
             val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)

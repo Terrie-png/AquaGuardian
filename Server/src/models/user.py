90e32debe440ce1af8f443e44a1f23f4c7a30b0c
kosystephy <kosysteve03@gmail.com>
2023-12-03 21:22:41 +0000

    Grant and UnGrant permissions to users
    Grant Permissions

--- Server/src/models/user.py
+++ Server/src/models/user.py
@@ -141,7 +141,38 @@ def get_all_logged_in_users():
                 str(row[n].authkey) + " | " +
                 str(row[n].login))
     return online_user_record
-    
+ 
+def bool_to_int(v):
+    if 'true' in str(v):
+        return 1
+    elif 'false' in str(v):
+        return 0
+    else:
+        raise ValueError
+
+
+def add_user_permission(u_id,name,email,password, read, write):
+    row = get_user_row_if_exists(u_id)
+    if row is not False:
+        row.read_access = bool_to_int(read)
+        row.write_access = bool_to_int(write)
+        db.session.commit()
+
+def get_user_access(user_id):
+    row = get_user_row_if_exists(user_id)
+    if row is not False:
+        get_user_row = User.query.filter_by(u_id=u_id).first()
+        read = get_user_row.read_access
+        if read == 1:
+            read = True
+        else:
+            read = False
+        write = get_user_row.write_access
+        if write == 1:
+            write = True
+        else:
+            write = False
+    return read, write   
         
 def __eq__(self, other):
         if isinstance(other, User):

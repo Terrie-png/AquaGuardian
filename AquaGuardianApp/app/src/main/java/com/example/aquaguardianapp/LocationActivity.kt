037cf3c6d8e964c30880a20537acf73c3b46e490
Raveena Rathnam <D00251856@student.dkit.ie>
2023-12-18 01:25:47 +0000

    Update LocationActivity.kt and activity_location.xml
    Created a map fragment and updated the layout.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/LocationActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/LocationActivity.kt
@@ -1,4 +1,28 @@
 package com.example.aquaguardianapp
 
-class LocationActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // Fetch locations using Retrofit and add markers
+        fetchLocationsAndMark()
+    }
+    private fun fetchLocationsAndMark() {
+
+    }
 }

31812202d640ab9940205296db1a0886a2430267
Raveena Rathnam <D00251856@student.dkit.ie>
2023-12-29 18:00:49 +0000

    Update HistoryActivity.kt
    Added the functionality of clear history only from the user interface and not in the backend as it only uses a mock API currently.

--- AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/HistoryActivity.kt
+++ AquaGuardianApp/app/src/main/java/com/example/aquaguardianapp/HistoryActivity.kt
@@ -17,20 +17,18 @@ import retrofit2.Response
 
 class HistoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHistoryBinding
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: HistoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        recyclerView = findViewById(R.id.rvHistoryList)
-        adapter = HistoryAdapter(listOf()) // Initialize with empty list
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        // Set up the RecyclerView with an empty adapter initially
+        binding.rvHistoryList.layoutManager = LinearLayoutManager(this)
+        val historyAdapter = HistoryAdapter(emptyList())
+        binding.rvHistoryList.adapter = historyAdapter
 
         val retrofit = ServiceBuilder.buildService(APIInterface::class.java)
-
         // Fetch history items
         retrofit.getHistory().enqueue(object : Callback<HistoryResponse> {
             override fun onResponse(
@@ -38,42 +36,40 @@ class HistoryActivity : AppCompatActivity() {
                 response: Response<HistoryResponse>
             ) {
                 if (response.isSuccessful) {
-                    val historyList = response.body()?.history ?: emptyList()
-                    adapter.items = historyList
-                    adapter.notifyDataSetChanged()
+                    // Update the adapter with the fetched history items
+                    historyAdapter.items = response.body()?.history ?: emptyList()
+                    historyAdapter.notifyDataSetChanged()
+                } else {
+                    Log.e("HistoryActivity", "Response not successful")
                 }
             }
-            override fun onFailure(call: Call<HistoryResponse>, t: Throwable) {
-                // Log the error as well, which is useful for debugging purposes
-                Log.e("HistoryActivity", "Error fetching history", t)
 
-                // Show an error message to the user
-                runOnUiThread {
-                    Toast.makeText(this@HistoryActivity, "Failed to fetch history: ${t.message}", Toast.LENGTH_LONG).show()
-                }
+            override fun onFailure(call: Call<HistoryResponse>, t: Throwable) {
+                Log.e("HistoryActivity", "Error fetching history", t)
+                Toast.makeText(this@HistoryActivity, "Failed to fetch history: ${t.message}", Toast.LENGTH_LONG).show()
             }
         })
 
-//        retrofit.getHistory().enqueue(
-//            object : Callback<HistoryResponse> { override fun onResponse(
-//                call: Call<HistoryResponse>,
-//                response: Response<HistoryResponse>
-//            ) {
-//                if (response.isSuccessful) {
-//                    // This is where you access 'history'
-//                    val historyList = response.body()?.history ?: emptyList()
-//                    // ... use historyList
-//                } else {
-//                    // Handle error...
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<HistoryResponse>, t: Throwable) {
-//                // Handle failure...
-//            }
-//        })
-
+        // Set the OnClickListener for the Clear History button
+        binding.btnClearHistory.setOnClickListener {
+            clearHistory()
+        }
     }
+
+    // Inside HistoryActivity
+    private fun clearHistory() {
+        // Clear the history from the UI
+        (binding.rvHistoryList.adapter as HistoryAdapter).items = emptyList()
+        binding.rvHistoryList.adapter?.notifyDataSetChanged()
+
+        // To clear the history from the backend,should make an API call
+        // val retrofit = ServiceBuilder.buildService(APIInterface::class.java)
+        // retrofit.clearHistory().enqueue(/* Implement the callback */)
+
+        // Show confirmation message
+        Toast.makeText(this, "History cleared", Toast.LENGTH_SHORT).show()
+    }
+
 }
 
 class HistoryAdapter(var items: List<HistoryItem>) : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
@@ -89,7 +85,7 @@ class HistoryAdapter(var items: List<HistoryItem>) : RecyclerView.Adapter<Histor
     }
 
     override fun getItemCount() = items.size
-    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val timeTextView: TextView = itemView.findViewById(R.id.tvTime)
         private val dateTextView: TextView = itemView.findViewById(R.id.tvDate)
         private val locationTextView: TextView = itemView.findViewById(R.id.tvLocation)
@@ -100,5 +96,4 @@ class HistoryAdapter(var items: List<HistoryItem>) : RecyclerView.Adapter<Histor
             locationTextView.text = historyItem.location
         }
     }
-
 }
